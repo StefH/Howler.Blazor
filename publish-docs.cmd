526f5a66610f592ca68ae81bae3c098c44b021ea
Stef Heyenrath <stef.heyenrath@gmail.com>
2020-05-05 14:57:30 +0200

    docs

--- publish-docs.cmd
+++ publish-docs.cmd
@@ -5,6 +5,11 @@ SET docs_folder=docs
 
 dotnet publish examples/Howler.Blazor-WASM-AudioPlayer -c Release -o %tmp_folder%
 
-RD /S /Q %docs_folder%
+rem delete all files except .gitattributes
+for /R "%docs_folder%" %%f in (*) do (if not "%%~xf"==".gitattributes" del "%%~f")
 
-robocopy %www_folder% %docs_folder% /E /MOVE
+rem delete all sub-folders
+for /d %%x in (%docs_folder%\*) do @rd /s /q "%%x"
+
+rem copy / move all from temp-docs into docs folder
+robocopy %www_folder% %docs_folder% /E /MOV
