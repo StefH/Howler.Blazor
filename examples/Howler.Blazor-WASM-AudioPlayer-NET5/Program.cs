174ed0b504555372603eecf752e3f646cf2ad9bb
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-02-08 16:06:52 +0000

    net5 example

---
+++ examples/Howler.Blazor-WASM-AudioPlayer-NET5/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Howler.Blazor.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Howler.Blazor_WASM_AudioPlayer_NET5
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<IHowl, Howl>();
+            builder.Services.AddScoped<IHowlGlobal, HowlGlobal>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
